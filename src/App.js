52d1f67c036113b43e45157c9b6c1d5b11685aef
mdadul <emdadulislam580@gmail.com>
2023-03-20 06:22:17 +0600

    fix error

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Courses from "./pages/Courses";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Error from "./components/common/Error";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/courses" element={<Courses />} />
-        <Route exact path='/signin' element={<SignIn />} />
-        <Route exact path='/signup' element={<SignUp />} />
-        <Route path="*" element={<Error/>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/courses" element={<Courses />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
